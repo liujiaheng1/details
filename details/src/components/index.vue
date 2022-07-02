72977a6918fac2adb30d721880f73156cccde696
liujiaheng <2537821834@qq.com>
2022-07-03 00:39:13 +0800

    createProject

---
+++ details/src/components/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {},
+
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped lang="scss">
+</style>
